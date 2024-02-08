afe07d29fec5d2c7dbd0c2016b566a84a6d5a7f2
Igor Sobral <igorgmz1421@gmail.com>
2024-02-08 12:39:42 -0300

    adding Toasters

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { Toaster} from "sonner"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors theme='system' className='transition-shadow' />
   </React.StrictMode>,
 )
